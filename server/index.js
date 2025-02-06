c8091aaff5c71c58b09a2ca03d4b5cdfaccf2892
razaqnejad <razaqnejad@gmail.com>
2025-02-06 13:59:51 +0330

    Refine HTTP to HTTPS redirection comment for clarity

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cors({
   methods: ["GET", "POST"]
 }));
 
-// Redirect HTTP to HTTPS (ONLY if Render is serving HTTP)
+// Redirect HTTP to HTTPS (Render is serving HTTP)
 app.use((req, res, next) => {
   if (req.headers["x-forwarded-proto"] !== "https") {
     return res.redirect("https://" + req.headers.host + req.url);
